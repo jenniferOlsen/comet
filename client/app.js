558be5fd9f5a15c22b9a2bafc8d6dc26f803b59d
Jenny Olsen <jenny.a.olsen@gmail.com>
2015-07-12 15:33:19 -0700

    finish part one

---
+++ client/app.js
@@ -0,0 +1,10 @@
+Template.messages.helpers({
+  messages: [
+    { text: "All these messages" },
+    { text: "Use the same template" },
+    { text: "But have different data" },
+    { text: "So they are different" },
+    { text: "HELLO" },
+    { text: "WORLD!" }
+  ]
+});
